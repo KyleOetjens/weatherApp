96dba8f0f96ff42bc81d38c04974df81a314f0aa
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-01-19 15:06:12 -0600

    commit

--- assets/script.js
+++ assets/script.js
@@ -46,15 +46,15 @@ function test() {
                     currentTempText.textContent = `Temp: High: ` + cityTempHigh + `°F` + `,   Low: ` + cityTempLow + `°F`
                     currentWindText.textContent = `Wind: ` + wind + ` MPH`
                     currentHumidText.textContent = `Humidity: ` + humid + `%`
-                   currentWeatherImg.src= `https://openweathermap.org/img/w/$`+weatherIcon + `.png`;
-                   console.log(currentWeatherImg);
-                   //var iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+                    currentWeatherImg.src = `https://openweathermap.org/img/w/` + weatherIcon + `.png`;
+                    console.log(currentWeatherImg);
+                    //var iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
                     getFiveDayForcast(lat, lon)
                 });
             }
         })
 }
-cityButton.on('click',test)
+cityButton.on('click', test)
 //think about generating 5 day forcast dynamically
 //get weather data from typed entry, need to use local storage
 //and generate a new button
@@ -71,56 +71,57 @@ const newCityWeather = function (event) {
         textCurrentWeather(cityType)
     } else {
         window.alert("please enter a valid city");
-    }}
-    let textCurrentWeather = function (cityType) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?appid=` + apiKey + `&q=` + cityType + `&units=imperial`)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        const lat = data.coord.lat
-                        const lon = data.coord.lon
-                        const cityName = data.name
-                        const cityTempHigh = data.main.temp_max
-                        const cityTempLow = data.main.temp_min
-                        const wind = data.wind.speed
-                        const humid = data.main.humidity
-                        const weatherIcon = data.weather[0].icon
-                        console.log(data);
-                        console.log(lat, lon, cityName, cityTempHigh, cityTempLow, wind, humid);
-                        currentCityText.textContent = cityName
-                        currentTempText.textContent = `Temp: High: ` + cityTempHigh + `°F` + `,   Low: ` + cityTempLow + `°F`
-                        currentWindText.textContent = `Wind: ` + wind + ` MPH`
-                        currentHumidText.textContent = `Humidity: ` + humid + `%`
-                        let newCityBtn = document.createElement('button');
-                       newCityBtn.addEventListener("click",test)
-                        document.querySelector ('.btn-group-vertical').appendChild(newCityBtn);
-                        newCityBtn.setAttribute("type", 'button');
-                        newCityBtn.setAttribute(`class`,`btn cityBtn btn-secondary`);
-                        newCityBtn.textContent = cityName;
-                        // consider let li = document.createElement('li')
-//document.querySelector('ul').appendChild(li)
-                        getFiveDayForcast(lat, lon)
-                    });
-                }
-            })
     }
-
-
-    userFormEl.addEventListener('submit', newCityWeather);
-
-
-
-
-
-
-
-
-
-
-
-    const getFiveDayForcast = function (lat, lon) {
-
-
-    }
-//const lat = data[0].lat //array
-  //                  const lon = data[0].lon
+}
+let textCurrentWeather = function (cityType) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=` + apiKey + `&q=` + cityType + `&units=imperial`)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    const lat = data.coord.lat
+                    const lon = data.coord.lon
+                    const cityName = data.name
+                    const cityTempHigh = data.main.temp_max
+                    const cityTempLow = data.main.temp_min
+                    const wind = data.wind.speed
+                    const humid = data.main.humidity
+                    const weatherIcon = data.weather[0].icon
+                    console.log(data);
+                    console.log(lat, lon, cityName, cityTempHigh, cityTempLow, wind, humid);
+                    currentCityText.textContent = cityName
+                    currentTempText.textContent = `Temp: High: ` + cityTempHigh + `°F` + `,   Low: ` + cityTempLow + `°F`
+                    currentWindText.textContent = `Wind: ` + wind + ` MPH`
+                    currentHumidText.textContent = `Humidity: ` + humid + `%`
+                    let newCityBtn = document.createElement('button');
+                    newCityBtn.addEventListener("click", test)
+                    document.querySelector('.btn-group-vertical').appendChild(newCityBtn);
+                    newCityBtn.setAttribute("type", 'button');
+                    newCityBtn.setAttribute(`class`, `btn cityBtn btn-secondary`);
+                    newCityBtn.textContent = cityName;
+                    currentWeatherImg.src = `https://openweathermap.org/img/w/` + weatherIcon + `.png`;
+                    // consider let li = document.createElement('li')
+                    //document.querySelector('ul').appendChild(li)
+                    getFiveDayForcast(lat, lon)
+                });
+            }
+        })
+}
+userFormEl.addEventListener('submit', newCityWeather);
+const getFiveDayForcast = function (lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + lon + `&units=imperial&appid=` + apiKey)
+        .then(function (response){
+            console.log("made it here");
+            if (response.ok) {
+                response.json().then(function (data){
+                    console.log(data);
+                    let date = data.list[2];
+                    console.log(date);
+                    for (let index = 2; index < data.length; index+8) {
+                        const element = data.list[index];
+                        console.log(element[index]);
+                        
+                    }
+                })
+            }
+        })
+}
